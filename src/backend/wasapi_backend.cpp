5975454e23cb4a0d23ddcf831bc666d1cf85af5b
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2019-03-20 00:43:22 +0100

    adds a few notes, subclass device_impl

--- src/backend/wasapi_backend.cpp
+++ src/backend/wasapi_backend.cpp
@@ -1,16 +1,30 @@
-#include <__audio_device_list>
 
 #ifdef LIBSTDAUDIO_BACKEND_WASAPI
 
+//win32
 #define NOMINMAX
 #include <windows.h>
+//IMMDevice
 #include <mmdeviceapi.h>
 #include <CommCtrl.h>
 #include <endpointvolume.h>
+#include <Audioclient.h> //WASAPI
+
+
+//Windows includes needs to be done before this one. 
+//Because there's confusion between Windows SDK's "byte" and std::byte, apparently.
+#include <__audio_device_list>
+#include "../device_impl.h"
+
 
 _LIBSTDAUDIO_NAMESPACE_BEGIN
+//TODO a device must be a wrapper around an IMMDevice 
 
-// Dummy implementation for platforms that do not support any audio devices
+
+class immdevice_wasapi_device_impl final : public device_impl
+{
+
+};
 
 device get_input_device() {
 	return {};
